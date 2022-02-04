cc6967d5195f17b096e5ad90fd6e9aa0c2e5953b
kolesov-alexandr <aleks.kolesov51@gmail.com>
2022-02-04 20:45:36 +0300

    Big task 2

--- Big task.py
+++ Big task.py
@@ -4,6 +4,9 @@ import Full_search
 
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+from io import BytesIO
+from PIL import Image
 
 
 SCREEN_SIZE = [600, 450]
@@ -13,8 +16,24 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.map_file = "map.png"
-        Full_search.search("Мурманск Софьи Перовской 5")
+        self.ll, self.spn = Full_search.search("Мурманск Софьи Перовской 5")
         self.initUI()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            lon, lat = map(float, self.spn.split(","))
+            lon -= 0.001
+            lat -= 0.001
+            self.spn = f"{lon},{lat}"
+        if event.key() == Qt.Key_PageUp:
+            lon, lat = map(float, self.spn.split(","))
+            lon += 0.001
+            lat += 0.001
+            self.spn = f"{lon},{lat}"
+        Full_search.make_map(Full_search.set_map_params(self.ll, self.spn))
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
@@ -24,9 +43,6 @@ class Example(QWidget):
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
-
-    def closeEvent(self, event):
-        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
